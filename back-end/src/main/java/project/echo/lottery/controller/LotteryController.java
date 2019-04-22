6c60a74a5e0ea9e00b656cb5f21074db692d9d97
kwm42 <2596123568@qq.com>
2019-04-22 16:30:31 +0800

    add lottery controller

--- back-end/src/main/java/project/echo/lottery/controller/LotteryController.java
+++ back-end/src/main/java/project/echo/lottery/controller/LotteryController.java
@@ -1,11 +1,13 @@
 package project.echo.lottery.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import project.echo.lottery.pojo.Award;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,6 +19,38 @@ public class LotteryController {
     public Map<String,Object> sayHello(){
         Map<String,Object> result=new HashMap<>();
         result.put("content","hello world");
+        return result;
+    }
+
+    @PostMapping
+    public Map<String,Object> lottery(@RequestBody Map<String,Object> info){
+        System.out.println(info);
+        Map<String,Object> result =new HashMap<>();
+
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date startTime= null;
+        Date endTime= null;
+        try {
+            startTime = sdf.parse((String)info.get("startTime"));
+            endTime = sdf.parse((String)info.get("endTime"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Boolean filterTeacher=(Boolean)info.get("filterTeacher");
+        Integer countLimit=(Integer)info.get("countLimit");
+        String keyWord=(String)info.get("keyWord");
+        String wenan=(String)info.get("wenan");
+        Boolean filterRepeat=(Boolean)info.get("filterRepeat");
+        List<Award> jx=(List<Award>)info.get("jx");
+
+        System.out.println("startTime=>"+startTime);
+        System.out.println("endTime=>"+endTime);
+        System.out.println("filterTeacher=>"+filterTeacher);
+        System.out.println("countLimit=>"+countLimit);
+        System.out.println("keyWord=>"+keyWord);
+        System.out.println("wenan=>"+wenan);
+        System.out.println("filterRepeat=>"+filterRepeat);
+        System.out.println("jx=>"+jx);
         return result;
     }
 }
---
+++ back-end/src/main/java/project/echo/lottery/service/LotteryService.java
@@ -0,0 +1,9 @@
+package project.echo.lottery.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LotteryService {
+
+    
+}
